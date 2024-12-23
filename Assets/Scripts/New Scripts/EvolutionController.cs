87633498fb92656fbccac43fd5838296d7da1cc5
kaylahee <kaylahee0410@gmail.com>
2024-12-24 01:05:46 +0900

    [feat] limit player play region (using camera)

---
+++ Assets/Scripts/New Scripts/EvolutionController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvolutionController : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject player0;
+    [SerializeField]
+    public GameObject player1;
+	[SerializeField]
+	public GameObject player2;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/New Scripts/PlayerController.cs
+++ Assets/Scripts/New Scripts/PlayerController.cs
@@ -26,54 +26,99 @@ public class PlayerController : MonoBehaviour
     private Vector2 _currentPosX;
 
     private float speed = 5f;
+    // 이동가능한 범위
+    private Vector2 p_moveLimit = new Vector2(4.0f, 4.0f);
+    [SerializeField]
+    private GameObject region;
 
-    public RectTransform front;
+	public RectTransform hp_front;
     private float _maxHp = 10f;
     private float _curHp = 10f;
 
-    /*private void Awake()
-    {
-        movement2D = GetComponent<Movement2D>();
-    }*/
+	public RectTransform score_front;
+	private float _maxScore = 9f;
+	private float _curScore = 0f;
 
-    private void Start()
+	private SpriteRenderer cur_player;
+	[SerializeField]
+	public Sprite player1;
+	[SerializeField]
+	public Sprite player2;
+
+    private Transform p_transform;
+
+	private void Start()
     {
-        
+        cur_player = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     private void Update()
     {
         Move();
-
         /*movement2D.MoveTo(new Vector3(x, y, 0));*/
     }
 
     // 방향 키를 눌러 이동 방향 설정
     private void Move()
     {
+        // 수직 이동
         float userInputV = Input.GetAxis("Vertical");
+        // 수평 이동
         float userInputH = Input.GetAxis("Horizontal");
 
-        Vector3 direction = new Vector3(userInputH, userInputV, 0);
+        Vector3 direction = new Vector3(-userInputH, userInputV, 0);
         transform.Translate(direction * speed * Time.deltaTime);
+        //transform.localPosition = ClampPosition(transform.localPosition);
+
+        Vector3 position = Camera.main.WorldToViewportPoint(transform.position);
+
+        if (position.x < 0f) position.x = 0f;
+        if (position.x > 1f) position.x = 1f;
+        if (position.y < 0f) position.y = 0f;
+        if (position.y > 1f) position.y = 1f;
+
+        transform.position = Camera.main.ViewportToWorldPoint(position);
     }
+
+    /*private Vector3 ClampPosition(Vector3 pos)
+    {
+        return new Vector3
+        (
+            // 좌우로 움직이는 이동범위
+            Mathf.Clamp(pos.x, -p_moveLimit.x, p_moveLimit.x),
+			Mathf.Clamp(pos.y, -p_moveLimit.y, p_moveLimit.y),
+            0
+		);
+    }*/
 
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.CompareTag("Feed"))
 		{
-			Debug.Log("Eat Feed");
+			Debug.Log(_curScore);
 			Destroy(collision.gameObject);
-            _curHp += 1f;
-			front.localScale = new Vector3(_curHp / _maxHp, 1.0f, 1.0f);
+            _curScore += 1f;
+			hp_front.localScale = new Vector3(_curHp / _maxHp, 1.0f, 1.0f);
+			score_front.localScale = new Vector3(_curScore / _maxScore, 1.0f, 1.0f);
+
+            if (_curScore == 3)
+            {
+                cur_player.sprite = player1;
+                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+			}
+            else if (_curScore == 5)
+            {
+                cur_player.sprite = player2;
+                transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+            }
 		}
 
         if (collision.CompareTag("Fishing"))
         {
             Debug.Log("Fishing");
             _curHp -= 1f;
-            front.localScale = new Vector3(_curHp / _maxHp, 1.0f, 1.0f);
+			hp_front.localScale = new Vector3(_curHp / _maxHp, 1.0f, 1.0f);
         }
 	}
 
